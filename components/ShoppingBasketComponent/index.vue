40a5262153b85c71953458da95f06bb4c7c6aeb9
sutipong.j <sutipong.j@ku.th>
2020-12-14 17:25:05 +0700

    oooooooo

--- components/ShoppingBasketComponent/index.vue
+++ components/ShoppingBasketComponent/index.vue
@@ -6,7 +6,7 @@
         <th class="sbcomponent_table_th">รายละเอียด</th>
         <th class="sbcomponent_table_th">ราคาต่อชิ้น</th>
         <th class="sbcomponent_table_th">จำนวน Jack</th>
-        <th class="sbcomponent_table_th">ราคารวม farmgodfuckingfit</th>
+        <th class="sbcomponent_table_th">ราคารวม oooooooo</th>
         <th class="sbcomponent_table_th"></th>
       </tr>
       <tr class="sbcomponent_table_th" v-for="(item, i) in Cart" :key="i">
